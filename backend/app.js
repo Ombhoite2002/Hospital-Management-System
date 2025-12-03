ad474689a39251950f9665bbec25fda6f5d522b0
Ombhoite2002 <om.bhoite2002@gmail.com>
2025-12-03 07:31:13 +0530

    Connect to DB

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDatabase = require('./db/db');
+
+connectToDatabase(); 
 
 app.use(cors());
 
